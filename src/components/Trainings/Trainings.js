a21f5fcd65a4cede5e343dd5b1877659a6e2faf5
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-12 00:56:47 +0200

    slide functions

--- src/components/Trainings/Trainings.js
+++ src/components/Trainings/Trainings.js
@@ -2,6 +2,18 @@ import TrainingItem from "../TrainingItem/TrainingItem";
 
 import "./Trainings.scss";
 
+const slideLeftHandler = () => {
+  let trainingsSelector = document.querySelector(".trainings");
+  let width = trainingsSelector.clientWidth;
+  trainingsSelector.scrollLeft = trainingsSelector.scrollLeft - width;
+};
+
+const slideRightHandler = () => {
+  let trainingsSelector = document.querySelector(".trainings");
+  let width = trainingsSelector.clientWidth;
+  trainingsSelector.scrollLeft = trainingsSelector.scrollLeft + width;
+};
+
 export default function Trainings() {
   return (
     <div className={"trainings"}>
