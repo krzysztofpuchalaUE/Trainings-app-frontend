30b4c070ed59a70ccdfb38a9cb1b88834a431a99
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-11 19:30:54 +0200

    Add routes: trainings, user-trainings, new-training, auth

--- src/App.js
+++ src/App.js
@@ -4,12 +4,43 @@ import ErrorPage from "./pages/Error";
 
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import UserTrainingsPage from "./pages/UserTrainings";
+import AuthenticationPage from "./pages/Authentication";
+import NewTrainingPage from "./pages/NewTraining";
+import TrainingsPage from "./pages/Trainings";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+        children: [
+          {
+            path: "trainings",
+            element: <TrainingsPage />,
+          },
+        ],
+      },
+      {
+        path: "user-trainings",
+        element: <UserTrainingsPage />,
+        children: [
+          {
+            path: "new-training",
+            element: <NewTrainingPage />,
+          },
+        ],
+      },
+      {
+        path: "auth",
+        element: <AuthenticationPage />,
+      },
+    ],
   },
 ]);
 
