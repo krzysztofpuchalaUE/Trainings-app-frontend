9b397099557897d707c3d5d8abffd0d9d547901d
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-11 21:04:22 +0200

    Setup NavLinks, add active link classes

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,22 +1,50 @@
+import { NavLink } from "react-router-dom";
+
 import "./Nav.scss";
 
 export default function NavBar() {
   return (
     <div className={"navbar"}>
-      <div className={"navbar-left"}>
-        <h2>Trainings App</h2>
-      </div>
+      <NavLink
+        to={"/"}
+        style={{ textDecoration: "none", color: "inherit" }}
+        className={({ isActive }) => (isActive ? "link-active" : undefined)}
+        end
+      >
+        <div className={"navbar-left"}>
+          <h2>Trainings App</h2>
+        </div>
+      </NavLink>
+
       <div className={"navbar-right"}>
-        <div className={"navbar-element"}>
-          <i class="bx bx-meteor"></i> <p>Trainings</p>
-        </div>
-        <div className={"navbar-element"}>
-          <i class="bx bx-face"></i>
-          <p>User trainings</p>
-        </div>
-        <div className={"navbar-element"}>
-          <i class="bx bx-user"></i> <p>Login</p>
-        </div>
+        <NavLink
+          to={"/trainings"}
+          className={({ isActive }) => (isActive ? "link-active" : undefined)}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <div className={"navbar-element"}>
+            <i class="bx bx-meteor"></i> <p>Trainings</p>
+          </div>
+        </NavLink>
+        <NavLink
+          to={"/user-trainings"}
+          className={({ isActive }) => (isActive ? "link-active" : undefined)}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <div className={"navbar-element"}>
+            <i class="bx bx-face"></i>
+            <p>User trainings</p>
+          </div>
+        </NavLink>
+        <NavLink
+          to={"/auth?mode=login"}
+          className={({ isActive }) => (isActive ? "link-active" : undefined)}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <div className={"navbar-element"}>
+            <i class="bx bx-user"></i> <p>Login</p>
+          </div>
+        </NavLink>
       </div>
     </div>
   );
