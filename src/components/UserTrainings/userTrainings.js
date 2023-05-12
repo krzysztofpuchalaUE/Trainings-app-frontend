cbdda09b6d0841261ca0e622a0822417b617c540
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-12 23:19:56 +0200

    Add right side of UserTrainings component

--- src/components/UserTrainings/userTrainings.js
+++ src/components/UserTrainings/userTrainings.js
@@ -12,7 +12,26 @@ export default function userTrainings() {
         <TrainingItem isUserTraining={true} />
         <TrainingItem isUserTraining={true} />
       </div>
-      <div className={"right"}></div>
+      <div className={"right"}>
+        <div className={"item-description"}>
+          <p>
+            asdpadawopdkopkawaw
+            dawdwaawddwawahuaegsrrrrrrrrrrauiiiiiiiiiiiiiiiiiiiwado
+          </p>
+          <div>
+            <div className={"item-features"}>
+              <div className={"update-training"}>
+                <i className={"bx bx-edit"}></i>
+                <p>Update</p>
+              </div>
+              <div className={"delete-training"}>
+                <i className={"bx bx-trash"}></i>
+                <p>Delete</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
