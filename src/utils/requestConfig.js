8f0e18ce68a8973fb1de1ecfb79389948a2f9259
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-13 23:37:58 +0200

    Create utils folder, put requests configs inside

---
+++ src/utils/requestConfig.js
@@ -0,0 +1,17 @@
+export const setConfig = (method, data) => {
+  const requestConfig = {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  return requestConfig;
+};
+
+export const requestGetConfig = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
