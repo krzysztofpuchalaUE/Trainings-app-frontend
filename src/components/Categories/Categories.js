70840641b2a31fb8f1ea331326ef6ef814599339
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-13 22:23:29 +0200

    Add Categories component, set dummy categories array

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,10 @@
+import "./Categories.scss";
+
+const categories = [
+  "data science",
+  "software engineering",
+  "game development",
+  "mobile development",
+];
+
+export default function Categories() {}
--- src/pages/Trainings.js
+++ src/pages/Trainings.js
@@ -1,5 +1,11 @@
 import Trainings from "../components/Trainings/Trainings";
+import Categories from "../components/Categories/Categories";
 
 export default function TrainingsPage() {
-  return <Trainings />;
+  return (
+    <>
+      <Categories />
+      <Trainings />;
+    </>
+  );
 }
