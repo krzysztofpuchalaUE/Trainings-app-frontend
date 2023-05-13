a6e5b45aac66b0de3a52bb1c1f9ff2e6b3d6587e
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-13 22:51:40 +0200

    Modify TrainingsPage, add categoryHandler and filter feature

--- src/pages/Trainings.js
+++ src/pages/Trainings.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
 import Trainings from "../components/Trainings/Trainings";
 import Categories from "../components/Categories/Categories";
 
 export default function TrainingsPage() {
+  const [trainingCategory, setTrainingCategory] = useState("");
+
+  const filterByCategory = (category) => {
+    setTrainingCategory(category);
+  };
+
+  let categoryURL = useParams();
+
   return (
     <>
-      <Categories />
-      <Trainings />;
+      <Categories setTrainingCategory={filterByCategory} />
+      <Trainings trainingCategory={categoryURL.category} />;
     </>
   );
 }
