1854a190f0476810d72c658234da98814138d481
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-14 14:08:58 +0200

    Create formatTrainingData.js to format data send by server

---
+++ src/utils/formatTrainingData.js
@@ -0,0 +1,27 @@
+const formatDate = (date) => {
+  const splitGenericDate = new Date(date.split("-").join(","));
+  return splitGenericDate.toLocaleDateString("pl-PL");
+};
+
+export const formatTrainingData = (item) => {
+  const newObj = {
+    id: item.id,
+    trainerId: item.trainer_id,
+    category: item.training_category,
+    description: item.training_description,
+    date: [
+      formatDate(item.training_start_date.split("T")[0]),
+      formatDate(item.training_end_date.split("T")[0]),
+    ].join("-"),
+    time: [
+      item.training_start_time.slice(0, 5),
+      item.training_end_time.slice(0, 5),
+    ].join("-"),
+    icon: item.training_icon,
+    language: item.training_language,
+    level: item.training_level,
+    location: item.training_location,
+    title: item.training_title,
+  };
+  return newObj;
+};
