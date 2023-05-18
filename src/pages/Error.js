038efef5a0dfb41cfa7a56796be559bf484f6698
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-19 00:09:04 +0200

    Modify error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,1 +1,24 @@
-export default function ErrorPage() {}
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+
+  const errorMessage = {};
+
+  if (error.status === 500) {
+    errorMessage.title = 500;
+    errorMessage.message = "Something went wrong";
+  }
+
+  if (error.status === 404) {
+    errorMessage.title = 404;
+    errorMessage.message = "Not found";
+  }
+
+  return (
+    <>
+      <h1>{errorMessage.title}</h1>
+      <h3>{errorMessage.message}</h3>
+    </>
+  );
+}
