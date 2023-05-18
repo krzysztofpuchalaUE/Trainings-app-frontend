18394019c9270b512ea64145b2dc49c3b85c749f
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-18 19:26:01 +0200

    Add getCategories query function, add http request for categories and display in Categories.js component

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,17 +1,47 @@
 import "./Categories.scss";
-import { categories } from "../../appConfig";
 
 import { Link, useParams } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { setConfig } from "../../utils/requestConfig";
+import useHttp from "../../hooks/useHttp";
+import { authContext } from "../../context/authContext";
 
 export default function Categories({ setTrainingCategory }) {
+  const [categories, setCategories] = useState([]);
+  const authCtx = useContext(authContext);
+
   const onChangeCategoryHandler = (category) => {
     setTrainingCategory(category);
   };
 
+  const {
+    requestForData: fetchCategories,
+    isLoading,
+    isError,
+  } = useHttp((value) => value);
+
+  useEffect(() => {
+    async function getCategories() {
+      const getCategories = await fetchCategories(
+        "http://localhost:8800/trainings",
+        setConfig("GET", null, true, authCtx.authToken)
+      );
+      const { allCategories } = getCategories;
+      console.log(allCategories);
+      setCategories(
+        allCategories.map((category) => {
+          return category.training_category;
+        })
+      );
+      console.log(categories);
+    }
+    getCategories();
+  }, []);
+
   return (
     <header className="main-header">
       <nav className="categories">
-        {categories.map((category) => (
+        {categories?.map((category) => (
           <Link
             to={`/trainings/${category}`}
             style={{ textDecoration: "none" }}
