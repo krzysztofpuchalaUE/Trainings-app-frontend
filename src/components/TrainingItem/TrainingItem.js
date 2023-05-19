40179946827e9280ec2d02ff459e6fc8924b68ae
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-19 20:39:51 +0200

    Fix bug, user can not unregister from created training

--- src/components/TrainingItem/TrainingItem.js
+++ src/components/TrainingItem/TrainingItem.js
@@ -1,4 +1,4 @@
-import { useCallback, useState, useContext } from "react";
+import { useCallback, useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { newTrainingItemContext } from "../../context/newTrainingItemContext";
@@ -19,6 +19,7 @@ export default function TrainingItem({
   const [userRegistered, setUserRegistered] = useState(
     item?.isRegistered || null
   );
+  const [isUserTrainer, setIsUserTrainer] = useState(false);
   const newTrainingItemCtx = useContext(newTrainingItemContext);
   const authCtx = useContext(authContext);
 
@@ -56,6 +57,26 @@ export default function TrainingItem({
     isLoading: postTrainingLoading,
     isError: postTrainingError,
   } = useHttp(applyPostData);
+
+  const {
+    requestForData: getUser,
+    isLoading: getUserLoading,
+    isError: getUserError,
+  } = useHttp((value) => value);
+
+  useEffect(() => {
+    async function getUserId() {
+      const data = await getUser(
+        "http://localhost:8800/trainings",
+        setConfig("GET", null, true, authCtx.authToken)
+      );
+      if (!data) return;
+      const { userId } = data;
+      const isTrainer = item?.trainerId !== userId[0].id;
+      setIsUserTrainer(isTrainer);
+    }
+    getUserId();
+  }, []);
 
   const onTrainingRegisterHandler = () => {
     const register = () => {
@@ -200,21 +221,23 @@ export default function TrainingItem({
           </div>
         )}
       </div>
-      {!isUserTraining && !isCreate && (
-        <div
-          className={`register-btn ${
-            !userRegistered ? undefined : "unregsiter"
-          }`}
-          onClick={onTrainingRegisterHandler}
-        >
-          {postTrainingLoading
-            ? "Processing..."
-            : userRegistered
-            ? "Unregister"
-            : "Register"}
-          {postTrainingError && " failed"}
-        </div>
-      )}
+      {!isUserTraining &&
+        !isCreate &&
+        isUserTrainer && ( // dokonczyc
+          <div
+            className={`register-btn ${
+              !userRegistered ? undefined : "unregsiter"
+            }`}
+            onClick={onTrainingRegisterHandler}
+          >
+            {postTrainingLoading
+              ? "Processing..."
+              : userRegistered
+              ? "Unregister"
+              : "Register"}
+            {postTrainingError && " failed"}
+          </div>
+        )}
     </div>
   );
 }
